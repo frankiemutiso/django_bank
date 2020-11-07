877aaf6a3e31e3e710a281308550fd9f54990767
Francis Mutiso <franckmutiso@gmail.com>
2020-11-07 12:03:06 +0300

    modified settings.py file

--- django_bank/settings.py
+++ django_bank/settings.py
@@ -84,7 +84,11 @@ WSGI_APPLICATION = 'django_bank.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
+        'HOST': env('DB_HOST'),
+        'PORT': env('DB_PORT')
     }
 }
 
