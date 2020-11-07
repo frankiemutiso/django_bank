2df9a945cd2aac6a12bd62d9450be4c042587158
Francis Mutiso <franckmutiso@gmail.com>
2020-11-07 21:46:36 +0300

    added psycopg to requirements

--- django_bank/settings.py
+++ django_bank/settings.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = 'django_bank.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': env('DB_NAME'),
         'USER': env('DB_USER'),
         'PASSWORD': env('DB_PASSWORD'),
